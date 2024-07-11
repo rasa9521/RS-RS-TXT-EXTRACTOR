b99644e35b5e5a58897b673307756323213d4d2b
kailashhh <alex.c992954@gmail.com>
2024-07-11 19:36:07 +0530

    TXT-EXTRACTOR

---
+++ config.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# (c) ACE 
+
+import os
+
+class Config(object):
+    # get a token from @BotFather
+    BOT_TOKEN = "6952010673:AAF2jLKLM9jHmG2b4KIHXrQEFPchvw0oMfo"
+    API_ID = 23636706
+    API_HASH = "569142e539afdf3b0739befbbcd8d8e2"
+    AUTH_USERS = 6935026876
